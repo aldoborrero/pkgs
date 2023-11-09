437e13a3cf3b41cad2851bacc105e6798150e295
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-11-09 19:58:28 +0100

    feat: add to pkgs/default.nix strip-tags and actual-server

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,29 +8,35 @@
     packages = let
       inherit (pkgs) callPackage python311Packages;
     in rec {
+      # by-name / ac
+      actual-server = callPackage ./by-name/ac/actual-server {};
+
       # by-name / fi
       filebrowser = callPackage ./by-name/fi/filebrowser {};
 
       # by-name / je
       jetbrains-fleet = callPackage ./by-name/je/jetbrains-fleet {};
 
-      # by-name/ os
+      # by-name / ll
+      llm = callPackage ./by-name/ll/llm {};
+
+      # by-name / os
       ospeak = callPackage ./by-name/os/ospeak {
         inherit openai; # requires openai version 1.0
       };
 
+      # by-name / sr
+      strip-tags = python311Packages.callPackage ./by-name/sr/strip-tags {};
+
       # by-name / sy
       systemctl-tui = callPackage ./by-name/sy/systemctl-tui {};
-
-      # by-name / ll
-      llm = callPackage ./by-name/ll/llm {};
 
       # development / python-modules
       pyemvue = callPackage ./development/python-modules/pyemvue {};
       openai = python311Packages.callPackage ./development/python-modules/openai {};
     };
 
-    apps = builtins.mapAttrs (_name: drv: lib.flakes.mkApp {inherit drv;}) self'.packages;
+    apps = lib.mapAttrs (_name: drv: lib.flakes.mkApp {inherit drv;}) self'.packages;
 
     overlayAttrs = self'.packages;
   };
