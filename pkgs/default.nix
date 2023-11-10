08e4f0b5b8e66d76a87232f047cee8109718029d
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-11-10 14:41:58 +0100

    feat: introduction of llm-clip

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -32,6 +32,7 @@
       systemctl-tui = callPackage ./by-name/sy/systemctl-tui {};
 
       # development / python-modules
+      llm-clip = python311Packages.callPackage ./development/python-modules/llm-clip {inherit llm;};
       llm-cluster = python311Packages.callPackage ./development/python-modules/llm-cluster {inherit llm;};
       llm-sentence-transformers = python311Packages.callPackage ./development/python-modules/llm-sentence-transformers {inherit llm;};
       pyemvue = callPackage ./development/python-modules/pyemvue {};
