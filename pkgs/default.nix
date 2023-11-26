0e00535ce77cf8433e5a3bc8384137fefc0292f2
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-11-26 13:50:08 +0100

    feat: comment projects that don't compile yet

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -36,10 +36,10 @@
       essentials = python311Packages.callPackage ./development/python-modules/essentials {};
       essentials-openapi = python311Packages.callPackage ./development/python-modules/essentials-openapi {inherit essentials;};
       gpt4all = python311Packages.callPackage ./development/python-modules/gpt4all {};
-      llm-claude = python311Packages.callPackage ./development/python-modules/llm-claude {inherit llm;};
+      # llm-claude = python311Packages.callPackage ./development/python-modules/llm-claude {inherit llm;};
       llm-clip = python311Packages.callPackage ./development/python-modules/llm-clip {inherit llm;};
       llm-cluster = python311Packages.callPackage ./development/python-modules/llm-cluster {inherit llm;};
-      llm-gpt4all = python311Packages.callPackage ./development/python-modules/llm-gpt4all {inherit llm;};
+      # llm-gpt4all = python311Packages.callPackage ./development/python-modules/llm-gpt4all {inherit llm;};
       llm-llama-cpp = python311Packages.callPackage ./development/python-modules/llm-llama-cpp {inherit llm;};
       llm-mlc = python311Packages.callPackage ./development/python-modules/llm-mlc {inherit llm;};
       llm-mpt30b = python311Packages.callPackage ./development/python-modules/llm-mpt30b {inherit llm ctransformers;};
@@ -49,7 +49,7 @@
       mlc-llm = python311Packages.callPackage ./development/python-modules/mlc-llm {};
       openai = python311Packages.callPackage ./development/python-modules/openai {};
       pydanticv2 = python311Packages.callPackage ./development/python-modules/pydanticv2 {};
-      pyemvue = callPackage ./development/python-modules/pyemvue {};
+      pyemvue = python311Packages.callPackage ./development/python-modules/pyemvue {};
     };
 
     apps = lib.mapAttrs (_name: drv: lib.flakes.mkApp {inherit drv;}) self'.packages;
