c72991b2d88827f4dd52fccded851fa15557e8b4
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2023-11-30 21:16:48 +0100

    feat: improve ollama module

--- modules/ollama.nix
+++ modules/ollama.nix
@@ -1,11 +1,41 @@
-{config, pkgs, lib, ...} : let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
   cfg = config.services.ollama;
 in {
   options.services.ollama = {
-    enable = lib.mkEnableOption "Ollama Server";
+    enable = mkEnableOption "Ollama Server";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.ollama;
+      description = "The Ollama package to use.";
+    };
+
+    host = mkOption {
+      type = types.str;
+      default = "127.0.0.1";
+      description = "Host for Ollama Server.";
+    };
+
+    port = mkOption {
+      type = types.str;
+      default = "11434";
+      description = "Port for Ollama Server.";
+    };
+
+    origins = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      description = "Origins for Ollama Server.";
+    };
   };
 
-  config = lib.mkIf cfg.enable {
+  config = mkIf cfg.enable {
     systemd.services.ollama = {
       description = "Ollama Server";
       after = ["network.target"];
@@ -13,8 +43,12 @@ in {
       StateDirectory = "ollama";
       DynamicUser = true;
       serviceConfig = {
-        execStart = "${pkgs.ollama}/bin/ollama serve";
+        execStart = "${cfg.package}/bin/ollama serve";
         Restart = "always";
+        Environment = [
+          "OLLAMA_HOST=${cfg.host}:${cfg.port}"
+          "OLLAMA_ORIGINS=${concatStringsSep "," cfg.origins}"
+        ];
       };
     };
   };
