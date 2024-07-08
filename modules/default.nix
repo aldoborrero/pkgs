174791ca1b759b3ab4bf850ed0fa0a2710f4aece
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-07-08 11:46:24 +0200

    feat: support adding independent nixos modules

--- modules/default.nix
+++ modules/default.nix
@@ -1,10 +1,24 @@
 {
-  # create a default nixos module which mixes in all modules
-  flake.nixosModules.default = {
-    imports = [
-      ./actual-server.nix
-      ./filebrowser.nix
-      ./pgweb.nix
-    ];
-  };
+  flake.nixosModules = let
+    # Get all .nix files in the current directory
+    files = builtins.attrNames (builtins.readDir ./.);
+
+    # Filter out any non-.nix files
+    nixFiles = builtins.filter (n: builtins.match ".*\\.nix" n != null) files;
+
+    # Remove the .nix extension
+    moduleNames = map (file: builtins.elemAt (builtins.split "\\." file) 0) nixFiles;
+
+    # Create an attribute set of module name to file path
+    modules = builtins.listToAttrs (map (name: {
+        inherit name;
+        value = import (./. + "/${name}.nix");
+      })
+      moduleNames);
+  in
+    # Add a 'default' module that imports all other modules
+    modules
+    // {
+      default = {imports = builtins.attrValues modules;};
+    };
 }
