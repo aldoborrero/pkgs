30ad264b4a2fe282f17ff0a357f5aa25e3a4b21a
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-11-29 16:21:21 +0100

    feat: no need for scopes

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,20 +5,15 @@
     lib,
     ...
   }: {
-    packages = let
-      scope = lib.makeScope pkgs.newScope (
-        _self:
-          lib.pipe (lib.packagesFromDirectoryRecursive {
-            inherit (pkgs) callPackage;
-            directory = ./by-name;
-          }) [
-            (lib.collect lib.isDerivation)
-            (map (x: lib.nameValuePair (builtins.head (builtins.split "-[0-9].*$" x.name)) x))
-            builtins.listToAttrs
-          ]
-      );
-    in
-      scope.overrideScope (_final: _prev: {});
+    packages =
+      lib.pipe (lib.packagesFromDirectoryRecursive {
+        inherit (pkgs) callPackage;
+        directory = ./by-name;
+      }) [
+        (lib.collect lib.isDerivation)
+        (map (x: lib.nameValuePair (builtins.head (builtins.split "-[0-9].*$" x.name)) x))
+        builtins.listToAttrs
+      ];
 
     overlayAttrs = self'.packages;
   };
