eba6bb04f2515e1ecb7b9bdce2a9c0d7a1ccd16a
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2024-12-19 17:00:33 +0100

    feat: use custom patched atlas to support postgresql unix socket connections

--- pkgs/by-name/di/digger-backend/package.nix
+++ pkgs/by-name/di/digger-backend/package.nix
@@ -2,45 +2,76 @@
   lib,
   buildGoModule,
   fetchFromGitHub,
-}:
-buildGoModule rec {
-  pname = "digger-backend";
-  version = "0.6.79";
+}: let
+  patchedAtlas = buildGoModule rec {
+    pname = "atlas";
+    version = "0.29.0";
 
-  src = fetchFromGitHub {
-    owner = "diggerhq";
-    repo = "digger";
-    rev = "v${version}";
-    hash = "sha256-/R52dwgKqpU9ffka5bz9xb7NyoCIu2/AgiWG0TT8nd0=";
+    src = fetchFromGitHub {
+      owner = "ariga";
+      repo = "atlas";
+      rev = "v${version}";
+      hash = "sha256-synQZAOnX5Xw5d7pHPr7eaycf/YErktCjlsPVwbyLks=";
+    };
+
+    modRoot = "cmd/atlas";
+
+    patches = [
+      ./atlas-postgres-sockets.patch
+    ];
+
+    proxyVendor = true;
+    vendorHash = "sha256-bQNcLFSMED5zFxf319fAeLLrVeZMCV/33s9hCm1elFs=";
+
+    ldflags = [
+      "-s"
+      "-w"
+      "-X ariga.io/atlas/cmd/atlas/internal/cmdapi.version=v${version}"
+    ];
+
+    subPackages = ["."];
   };
+in
+  buildGoModule rec {
+    pname = "digger-backend";
+    version = "0.6.79";
 
-  vendorHash = "sha256-qcItUM2wQ4fgFDMGkyymxQugGaRQvn7rrmSzaLtL76Q=";
-  proxyVendor = true;
+    src = fetchFromGitHub {
+      owner = "diggerhq";
+      repo = "digger";
+      rev = "v${version}";
+      hash = "sha256-/R52dwgKqpU9ffka5bz9xb7NyoCIu2/AgiWG0TT8nd0=";
+    };
 
-  CGO_ENABLED = 0;
+    vendorHash = "sha256-qcItUM2wQ4fgFDMGkyymxQugGaRQvn7rrmSzaLtL76Q=";
+    proxyVendor = true;
 
-  ldflags = [
-    "-s"
-    "-w"
-    "-X main.Version=${version}"
-  ];
+    CGO_ENABLED = 0;
 
-  subPackages = ["backend"];
+    ldflags = [
+      "-s"
+      "-w"
+      "-X main.Version=${version}"
+    ];
 
-  postInstall = ''
-    mv $out/bin/backend $out/bin/digger-backend
+    subPackages = ["backend"];
 
-    # copy migrations
-    mkdir -p $out/share/
-    cp -r $src/backend/migrations $out/share/
-    cp -r $src/backend/templates $out/share/
-  '';
+    postInstall = ''
+      mv $out/bin/backend $out/bin/digger-backend
 
-  meta = with lib; {
-    description = "Backend service for Digger, an open source IaC orchestration tool";
-    homepage = "https://github.com/diggerhq/digger";
-    license = licenses.asl20;
-    mainProgram = "digger-backend";
-    maintainers = with maintainers; [aldoborrero];
-  };
-}
+      # copy migrations
+      mkdir -p $out/share/
+      cp -r $src/backend/migrations $out/share/
+      cp -r $src/backend/templates $out/share/
+    '';
+
+    passthru.atlas = patchedAtlas;
+
+    meta = with lib; {
+      description = "Backend service for Digger, an open source IaC orchestration tool";
+      homepage = "https://github.com/diggerhq/digger";
+      license = licenses.asl20;
+      mainProgram = "digger-backend";
+      maintainers = with maintainers; [aldoborrero];
+    };
+  }
